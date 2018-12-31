e5207eb5e18d27a55c1ba4ea4d290841f33b18f8
pauaskara@gmail.com <pauaskara@gmail.com>
2018-12-31 21:16:53 +0530

    modified the solution to find the total subsequences with product less than k, to accomodate sequences with zero product

--- Problems/count_subseq.cpp
+++ Problems/count_subseq.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 int count_subseq(int a[], int n, int k, int product) {
 
   // if no items left, return 0
   if(n < 0) return 0;
+  // don't consider zero element, their sum is added separately
+  else if(a[n] == 0) return count_subseq(a, n-1, k, product);
   // if the current element is greater than required product, dont consider it, recurse for the other elements
   else if(a[n] > k) return count_subseq(a, n-1, k, product);
   // if current element can be included, add it and also recurse for solutions without the current element
@@ -13,22 +15,28 @@ int count_subseq(int a[], int n, int k, int product) {
 }
 
 int main() {
-  int n,k, ans = 0;
+  int n,k, ans = 0, zeros = 0;
 
   std::cin>>n>>k;
 
   int a[n];
 
-  for(int i=0;i<n;i++) std::cin>>a[i];
+  for(int i=0;i<n;i++) {
+    std::cin>>a[i];
+    if(a[i] == 0) zeros++;
+  }
 
 
   for(int i = n-1;i>=0;i--) {
-    if(a[i] < k) {
+    if(a[i] < k && a[i] > 0) {
       // single element sequence
       ans++;
       ans += count_subseq(a, i-1, k, a[i]);
     }
   }
+  
+  // no of zero product sequences
+  if(zeros > 0) ans += (std::pow(2, zeros) - 1 /* possibility of no zeros being selected is discarded */) * std::pow(2, n - zeros) + 1;
 
   std::cout<<ans<<std::endl;
 
