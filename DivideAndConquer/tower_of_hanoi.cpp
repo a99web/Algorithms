0d942dc9553bc2d44c85a19ab81c3f3ea4a5dda0
pauaskara@gmail.com <pauaskara@gmail.com>
2019-02-04 21:13:34 +0530

    fixed typo, toi--->toh

--- DivideAndConquer/tower_of_hanoi.cpp
+++ DivideAndConquer/tower_of_hanoi.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 
-void toi(int n, char start, char auxi, char dest) {
+void toh(int n, char start, char auxi, char dest) {
   if(n > 0) {
-    toi(n-1, start, dest, auxi);
+    toh(n-1, start, dest, auxi);
     std::cout<<"Moving disk from "<<start<<" to "<<dest<<std::endl;
-    toi(n-1, auxi, start, dest);
+    toh(n-1, auxi, start, dest);
   }
 }
 
 
 int main() {
   int n = 3;
-  toi(n, 'A', 'B', 'C');
+  toh(n, 'A', 'B', 'C');
   return 0;
 }
